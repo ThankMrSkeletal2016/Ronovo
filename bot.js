140dac5815a4ce7054641f68ffabc23ba53280d0
ThankMrSkeletal2016 <ThankMrSkeletal2016@gmail.com>
2018-03-05 21:05:30 -0600

    Adding Multi Dice
    Multi dice, with DCC dice. Also, updated help command to reflect the new changes.
    Ronovo V0.1 is live

--- bot.js
+++ bot.js
@@ -7,13 +7,11 @@ const config = require("./config.json");
 const sql = require("sqlite");
 sql.open("./score.sqlite");
 
-// This loop reads the /events/ folder and attaches each event file to the appropriate event.
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
     let eventFunction = require(`./events/${file}`);
     let eventName = file.split(".")[0];
-    // super-secret recipe to call events with all their proper arguments *after* the `client` var.
     client.on(eventName, (...args) => eventFunction.run(client, ...args));
   });
 });
@@ -22,11 +20,9 @@ client.on("message", message => {
   if (message.author.bot) return;
   if(message.content.indexOf(config.prefix) !== 0) return;
 
-  // This is the best way to define args. Trust me.
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  // The list of if/else is replaced with those simple 2 lines:
   try {
     let commandFile = require(`./commands/${command}.js`);
     commandFile.run(client, message, args);
@@ -37,14 +33,15 @@ client.on("message", message => {
     if (!row) {
       sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
     } else {
-      let curLevel = Math.floor(0.1 * Math.sqrt(row.points + 1));
+      let curLevel = Math.floor(0.1 * Math.sqrt(row.points));
       if (curLevel > row.level) {
         row.level = curLevel;
         sql.run(`UPDATE scores SET points = ${row.points + 1}, level = ${row.level} WHERE userId = ${message.author.id}`);
         message.reply(`You've leveled up to level **${curLevel}**! Ain't that dandy?`);
-      }
+      }else{
       sql.run(`UPDATE scores SET points = ${row.points + 1} WHERE userId = ${message.author.id}`);
-    }
+	  }	
+	}
   }).catch(() => {
     console.error;
     sql.run("CREATE TABLE IF NOT EXISTS scores (userId TEXT, points INTEGER, level INTEGER)").then(() => {
--- commands/roll.js
+++ commands/roll.js
@@ -1,3 +1,16 @@
+//Random Flag I use for conditionals. Will Give it a name at some point.
+//#BadCodingHabits
+var flag = false;
+
+//All Of The Dice We will Be Using
+var d3 = {
+  sides: 3,
+  roll: function () {
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+  }
+}
+
 var d4 = {
   sides: 4,
   roll: function () {
@@ -6,8 +19,24 @@ var d4 = {
   }
 }
 
+var d5 = {
+  sides: 5,
+  roll: function () {
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+  }
+}
+
 var d6 = {
   sides: 6,
+  roll: function () {
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+  }
+}
+
+var d7 = {
+  sides: 7,
   roll: function () {
     var randomNumber = Math.floor(Math.random() * this.sides) + 1;
     return randomNumber;
@@ -38,6 +67,22 @@ var d12 = {
   }
 }
 
+var d14 = {
+  sides: 14,
+  roll: function () {
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+  }
+}
+
+var d16 = {
+  sides: 16,
+  roll: function () {
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+  }
+}
+
 var d20 = {
   sides: 20,
   roll: function () {
@@ -46,7 +91,23 @@ var d20 = {
   }
 }
 
-var percent = {
+var d24 = {
+  sides: 20,
+  roll: function () {
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+  }
+}
+
+var d30 = {
+  sides: 20,
+  roll: function () {
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+  }
+}
+
+var d100 = {
   sides: 100,
   roll: function () {
     var randomNumber = Math.floor(Math.random() * this.sides) + 1;
@@ -54,30 +115,103 @@ var percent = {
   }
 }
 
-exports.run = (client, message, args) => {
-     switch(args[0]) {
-			case "d4":
-				var result = d4.roll();
-			break;
-			case 'd6':
-				var result = d6.roll();
-			break;
-			case 'd8':
-				var result = d8.roll();
-			break;
-			case 'd10':
-				var result = d10.roll();
-			break;
-			case 'd12':
-				var result = d12.roll();
-			break;
-			case 'd20':
-				var result = d20.roll();
-			break;
-			case 'percent':
-				var result = percent.roll();
-			break;
+function rollDice(string) {
+	var result = 0;
+	switch(string) {
+		case "d3":
+			 result = d3.roll();
+		break;
+		case "d4":
+			 result = d4.roll();
+		break;
+		case "d5":
+			 result = d5.roll();
+		break;
+		case 'd6':
+			 result = d6.roll();
+		break;
+		case "d7":
+			 result = d7.roll();
+		break;
+		case 'd8':
+			 result = d8.roll();
+		break;
+		case 'd10':
+			 result = d10.roll();
+		break;
+		case 'd12':
+			 result = d12.roll();
+		break;
+		case 'd14':
+			 result = d14.roll();
+		break;
+		case 'd16':
+			 result = d16.roll();
+		break;
+		case 'd20':
+			 result = d20.roll();
+		break;
+		case 'd24':
+			 result = d24.roll();
+		break;
+		case 'd30':
+			var result = d30.roll();
+		break;
+		case 'd100':
+			var result = d100.roll();
+		break;
+		default:
+			flag = true;
 	 }
-	var msg = "You rolled a " + result;
-	message.channel.send(msg).catch(console.error);
+	 return result
+}
+
+exports.run = (client, message, args) => {
+	var firstChar = args[0].charAt(0);
+	//First Option : They send a single die
+	if(firstChar == 'd'){
+		var result = rollDice(args[0]);
+		if(!flag){
+			var msg = "You rolled a " + result;
+		} else {
+			var msg = "That is not a valid roll."
+		}
+		flag = false;
+		message.channel.send(msg).catch(console.error);
+	} else {
+		//Second option : They send multiple dice
+		if(firstChar <='9' && firstChar >='0') {
+			var validFlag = true;
+			var x = firstChar;
+			if(args[0].length == 3){
+				var die = args[0].substring(1,3)
+			} else if(args[0].length == 4){
+				var die = args[0].substring(1,4)
+			} else if(args[0].length == 5){
+				var die = args[0].substring(1,5)
+			}
+			var result = 0;
+			var total = 0;
+			for(i = 0; i < x; i++){
+				result = rollDice(die);
+				//if flag is true, die type sent was not valid
+				if(flag){
+					var msg = "That is not a valid die type."
+					message.channel.send(msg).catch(console.error);
+					flag = false
+					validFlag = false;
+					break;
+				}
+				total = total + result;
+			}
+			if(validFlag){
+				var msg = "You rolled " + args[0] + " for a total of " + total+"!";
+				message.channel.send(msg).catch(console.error);	
+			}
+		//Final Option : They didn't send a valid string
+		} else{
+			var msg = "Your roll must begin with a number or d!"
+			message.channel.send(msg).catch(console.error);
+		}
+	}
 }
